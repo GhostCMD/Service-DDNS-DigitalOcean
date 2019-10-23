d9e49f4e59e0c4911f823134cfc2ab9dbda7f15a
GlitchWraith <GlitchWraith@ghostlink.net>
2019-10-23 22:01:50 +0100

    ShellCheck checker valid
    Signed-off-by: GlitchWraith <GlitchWraith@ghostlink.net>

--- DDNS-Timer.sh
+++ DDNS-Timer.sh
@@ -1,56 +1,65 @@
+#!/bin/sh
+
 Update(){
-	Current_IP=`curl -s http://ipv4bot.whatismyipaddress.com/`
-	curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d "{\"data\":\"$Current_IP\"}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$ID" | jq
- 
+        Current_IP=$(curl -s http://ipv4bot.whatismyipaddress.com/)
+        curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d "{\"data\":\"$Current_IP\"}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$ID" | jq
 }
 
 
 make_dns_record(){
-	IP=`curl -s http://ipv4bot.whatismyipaddress.com/`
-	curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d "{\"type\":\"A\",\"name\":\"$DATA\",\"data\":\"$IP\",\"priority\":null,\"port\":null,\"ttl\":1800,\"weight\":null,\"flags\":null,\"tag\":null}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records" | jq '.domain_record.id' > /tmp/id
-	export ID=`cat /tmp/id`
-	rm -f /tmp/id
+        IP=$(curl -s http://ipv4bot.whatismyipaddress.com/)
+        curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d "{\"type\":\"A\",\"name\":\"$DATA\",\"data\":\"$IP\",\"priority\":null,\"port\":null,\"ttl\":1800,\"weight\":null,\"flags\":null,\"tag\":null}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records" | jq '.domain_record.id' > /tmp/id
+        ID=$(cat /tmp/id)
+        export ID
+        rm -f /tmp/id
 }
 
 
 get_dnsObjects(){
-	curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records" > /tmp/objects.json 
+        curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records" > /tmp/objects.json 
 
 }
 
 enumurateObjects(){
-	cat /tmp/objects.json | jq '.domain_records | length'
+        </tmp/objects.json  jq '.domain_records | length'
+        return 0
 }
 
 get_name(){
-	return cat /tmp/objects.json | jq '.domain_records['$1'].name'
-}	
+        </tmp/objects.json  jq '.domain_records['"$1"'].name'
+        return 0
+}
 get_type(){
-	return cat /tmp/objects.json | jq '.domain_records['$1'].type'
+        </tmp/objects.json  jq '.domain_records['"$1"'].type'
+        return 0
 }
 match_name(){
-	for i in enumurateObjects ;
-	do 
-		if [ $DATA  = ( get_name $i) && "\"A\"" = (get_type $i)  ];then
-			return $i
-		fi
-
-	done
-	return -1
-
+        for i in $(enumurateObjects) ;
+        do 
+                Name=$(get_name "$i")
+                Type=$(get_type "$i")
+                AType="\"A\""
+                if [ "$DATA"  =  "$Name"  ]; then
+                        if [ "$Type" = "$AType" ];then
+                                return "$i"
+                                break
+                        fi
+                fi
+        done
+        return 255
 }
 
 get_dnsObjects
 
-if [ match_name = -1 ]; then
-	make_dns_record
+if [ "$(match_name)" = 255 ]; then
+        make_dns_record
 else
-	export ID=`cat /tmp/objects.json | jq '.domain_records['match_name'].id'`
-
+        ID=$( </tmp/objects.json  jq '.domain_records['"$(match_name)"'].id')
+        export ID
 fi
 
 
 while true; do
-	Update
-	sleep 6h
-	done
+        Update
+        sleep 6h
+        done
